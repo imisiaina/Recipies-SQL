f3a425b932de70b2006b166b7f1cf993e9947348
imisiaina <imis.aina1@gmail.com>
2022-09-29 16:03:38 +0100

    Project Setup

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require_relative 'lib/database_connection'
 
 # We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+DatabaseConnection.connect('recipies_directory')
 
 # Perform a SQL query on the database and get the result set.
 sql = 'SELECT id, title FROM albums;'
